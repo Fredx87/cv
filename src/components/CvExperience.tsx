38caae6ad1febb9626eeaf84fe22ff726d8cbfd6
Fredx <freddy87@gmail.com>
2019-11-26 00:21:51 +0100

    feat: Refactored CvExperience component to support work experience and university experience

--- src/components/CvExperience.tsx
+++ src/components/CvExperience.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-export interface CvExperienceProps {
+export interface CvExperienceCommonProps {
   start: string;
   end: string;
-  company?: string;
-  place?: string;
   children: React.ReactNode;
 }
+
+export interface CvWorkExperienceProps extends CvExperienceCommonProps {
+  type: "work";
+  company: string;
+  place: string;
+}
+
+export interface CvUniversityExperienceProps extends CvExperienceCommonProps {
+  type: "university";
+  university: string;
+  finalMark: string;
+}
+
+export type CvExperienceProps =
+  | CvWorkExperienceProps
+  | CvUniversityExperienceProps;
 
 const Container = styled.div`
   display: flex;
@@ -15,6 +29,7 @@ const Container = styled.div`
 
 const Side = styled.div`
   flex: 0 20rem;
+  margin-right: 2rem;
 `;
 
 const Content = styled.div`
@@ -22,14 +37,34 @@ const Content = styled.div`
 `;
 
 export function CvExperience(props: CvExperienceProps) {
+  function getSideContent(): JSX.Element {
+    switch (props.type) {
+      case "work": {
+        return (
+          <>
+            <p>🏢 {props.company}</p>
+            <p>📌 {props.place}</p>
+          </>
+        );
+      }
+      case "university": {
+        return (
+          <>
+            <p>🎓 {props.university}</p>
+            <p>📜 Final mark: {props.finalMark}</p>
+          </>
+        );
+      }
+    }
+  }
+
   return (
     <Container>
       <Side>
         <p>
           📅 {props.start} &ndash; {props.end}
         </p>
-        {props.company ? <p>🏢 {props.company}</p> : null}
-        {props.place ? <p>📌 {props.place}</p> : null}
+        {getSideContent()}
       </Side>
       <Content>{props.children}</Content>
     </Container>
