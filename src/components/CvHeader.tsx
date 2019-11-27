f9bf9e95375baf116a31834dcc3c93f1e85985d1
Fredx <freddy87@gmail.com>
2019-11-28 00:40:21 +0100

    feat: Added CvHeader component

---
+++ src/components/CvHeader.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+
+export interface CvHeaderProps {
+  photoUrl: string;
+  name: string;
+  phoneNumber?: string;
+  email: string;
+  place?: string;
+  birthDate?: string;
+}
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Photo = styled.img`
+  border-radius: 100%;
+  width: 30mm;
+  height: 30mm;
+  margin-right: 1rem;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  h1 {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+    border-bottom: 1px solid #000000;
+    letter-spacing: 2;
+    padding: 1.15rem 0;
+    margin: 0;
+  }
+
+  h1 small {
+    font-variant: small-caps;
+    font-size: 0.8em;
+    color: #999999;
+  }
+`;
+
+const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1.15rem 0;
+`;
+
+function getEmailLink(email: string) {
+  const link = `mailto:${email}`;
+  return <a href={link}>{email}</a>;
+}
+
+export function CvHeader(props: CvHeaderProps) {
+  return (
+    <Container>
+      <Photo src={props.photoUrl}></Photo>
+      <Right>
+        <h1>
+          {props.name}
+          <small>Curriculum Vitae</small>
+        </h1>
+        <InfoContainer>
+          {props.place ? <span> 📌 {props.place}</span> : null}
+          {props.phoneNumber ? <span>📞 {props.phoneNumber} </span> : null}
+          {props.email ? <span> ✉️ {getEmailLink(props.email)}</span> : null}
+        </InfoContainer>
+      </Right>
+    </Container>
+  );
+}
