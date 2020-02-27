d03ec3928e15aadc8a4827ad13a4fc3039ddab04
Fredx <freddy87@gmail.com>
2020-02-27 22:17:48 +0100

    Added theme-ui in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
+// @ts-ignore
+import { toTheme } from "@theme-ui/typography";
 import React from "react";
 import { render } from "react-dom";
 import Twemoji from "react-twemoji";
+import { ThemeProvider } from "theme-ui";
+// @ts-ignore
+import fairyGates from "typography-theme-fairy-gates";
 import Content from "./cv.mdx";
-import "./styles.css";
+
+const theme = toTheme(fairyGates);
 
 render(
-  <Twemoji options={{ folder: "svg", ext: ".svg" }}>
-    <Content />
-  </Twemoji>,
+  <ThemeProvider theme={theme}>
+    <Twemoji options={{ folder: "svg", ext: ".svg" }}>
+      <Content />
+    </Twemoji>
+  </ThemeProvider>,
   document.querySelector("#container")
 );
