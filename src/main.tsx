86d70299a92fcb1b3d503bf05d1c142773e4ff4d
Fredx <freddy87@gmail.com>
2020-02-28 00:13:13 +0100

    Fixed global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 // @ts-ignore
 import { toTheme } from "@theme-ui/typography";
-import React from "react";
+import merge from "deepmerge";
 import { render } from "react-dom";
 import Twemoji from "react-twemoji";
 import { ThemeProvider } from "theme-ui";
@@ -8,11 +10,42 @@ import { ThemeProvider } from "theme-ui";
 import fairyGates from "typography-theme-fairy-gates";
 import Content from "./cv.mdx";
 
-const theme = toTheme(fairyGates);
+fairyGates.baseFontSize = "16px";
+
+const theme = merge(toTheme(fairyGates), {
+  colors: {
+    text: "hsla(0,0%,0%,0.8)",
+    background: "#fff",
+    primary: "#1ca086",
+    muted: "#999999"
+  },
+  styles: {
+    root: {
+      maxWidth: "170mm",
+      margin: "0 auto"
+    }
+  },
+  images: {
+    avatar: {
+      width: 120,
+      height: 120,
+      borderRadius: "100%"
+    }
+  }
+});
 
 render(
   <ThemeProvider theme={theme}>
-    <Twemoji options={{ folder: "svg", ext: ".svg" }}>
+    <Twemoji
+      options={{ folder: "svg", ext: ".svg" }}
+      css={css`
+        img.emoji {
+          width: 1rem;
+          vertical-align: text-top;
+          margin-right: 0.5rem;
+        }
+      `}
+    >
       <Content />
     </Twemoji>
   </ThemeProvider>,
