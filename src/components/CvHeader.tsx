07f6df79bd1611f25b38f1d3420f9f3241bebcb9
Fredx <freddy87@gmail.com>
2020-02-27 22:21:04 +0100

    Refactored Header with theme-ui

--- src/components/CvHeader.tsx
+++ src/components/CvHeader.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+/** @jsx jsx */
+import { Avatar, Box, Flex, jsx } from "theme-ui";
 
 export interface CvHeaderProps {
   photoUrl: string;
@@ -10,40 +10,6 @@ export interface CvHeaderProps {
   birthDate?: string;
 }
 
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Photo = styled.img`
-  border-radius: 100%;
-  width: 10rem;
-  height: 10rem;
-  margin-right: 1rem;
-`;
-
-const Right = styled.div`
-  flex: 1;
-  h1 {
-    display: flex;
-    justify-content: space-between;
-    align-items: baseline;
-    border-bottom: 1px solid #000000;
-    padding: 0.5rem 0;
-    margin: 0;
-  }
-
-  h1 small {
-    font-variant: small-caps;
-    font-size: 0.8em;
-    color: #999999;
-  }
-`;
-
-const InfoContainer = styled.div`
-  padding: 0.5rem 0;
-`;
-
 function getEmailLink(email: string) {
   const link = `mailto:${email}`;
   return <a href={link}>{email}</a>;
@@ -51,19 +17,19 @@ function getEmailLink(email: string) {
 
 export function CvHeader(props: CvHeaderProps) {
   return (
-    <Container>
-      <Photo src={props.photoUrl}></Photo>
-      <Right>
+    <Flex>
+      <Avatar src={props.photoUrl}></Avatar>
+      <Box sx={{ flex: 1 }}>
         <h1>
           {props.name}
           <small>Curriculum Vitae</small>
         </h1>
-        <InfoContainer>
+        <Box>
           {props.place ? <div> 📌 {props.place}</div> : null}
           {props.phoneNumber ? <div>📞 {props.phoneNumber} </div> : null}
           {props.email ? <div> ✉️ {getEmailLink(props.email)}</div> : null}
-        </InfoContainer>
-      </Right>
-    </Container>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
